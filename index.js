962282dcc4ed61b3aee905db6e284c1897c59d90
J a w S <119134061+jawsogood@users.noreply.github.com>
2024-10-04 01:43:51 +0300

    jawSo

---
+++ index.js
@@ -0,0 +1,15 @@
+const config = require('./config.js');
+const keep_alive = require('./keep_alive.js')
+
+if(config.shardManager.shardStatus == false){
+
+const { ShardingManager } = require('discord.js');
+const manager = new ShardingManager('./bot.js', { token: config.TOKEN || process.env.TOKEN });
+manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`));
+manager.spawn();
+
+} else {
+
+require("./bot.js")
+
+}
